7cbd219eee311e83e2daeecb9e20c9873bd66749
Mr-Bossman <mr.bossman075@gmail.com>
2021-01-21 15:06:07 -0500

    blueberry

--- backend.js
+++ backend.js
@@ -1,22 +1,53 @@
 const express = require('express');
 const app = express();
-
-
-
+var fs = require('fs');
 
 app.get('/', (req, res, next) => {
-  res.sendfile('./leekspin.com.html');
+  res.sendfile('./index.html');
 })
 app.get('/image', (req, res, next) => {
   res.sendfile('./image.gif');
 })
-app.get('/polkka.mp3', (req, res, next) => {
+app.get('/blueberry', function(req, res) {
+  const path = 'sample.mp4'
+  const stat = fs.statSync(path)
+  const fileSize = stat.size
+  const range = req.headers.range
+  if (range) {
+    const parts = range.replace(/bytes=/, "").split("-")
+    const start = parseInt(parts[0], 10)
+    const end = parts[1] 
+      ? parseInt(parts[1], 10)
+      : fileSize-1
+    const chunksize = (end-start)+1
+    const file = fs.createReadStream(path, {start, end})
+    const head = {
+      'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': chunksize,
+      'Content-Type': 'video/mp4',
+    }
+    res.writeHead(206, head);
+    file.pipe(res);
+  } else {
+    const head = {
+      'Content-Length': fileSize,
+      'Content-Type': 'video/mp4',
+    }
+    res.writeHead(200, head)
+    fs.createReadStream(path).pipe(res)
+  }
+});
+app.get('/menu', (req, res, next) => {
+  res.sendfile('./menu.png');
+})
+app.get('/music.mp3', (req, res, next) => {
   res.sendfile('./polkka.mp3');
 })
-app.get('/info.txt', (req, res, next) => {
+app.get('/about', (req, res, next) => {
   res.sendfile('./info.txt');
 })
-app.get('/polkka.ogg', (req, res, next) => {
+app.get('/music.ogg', (req, res, next) => {
   res.sendfile('./polkka.ogg');
 })
 app.get('/icon.png', (req, res, next) => {
